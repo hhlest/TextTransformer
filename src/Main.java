09279ffec4b1f74a4213dd3708c07b6142e543d1
Waminms <67516571+Waminms@users.noreply.github.com>
2023-01-09 15:29:28 +0200

    Extra toString methods deleted

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         String test = "Loremipsum";
+
         TextTransformer myTextTransformer = new TextTransformer();
         InverseTransformer myInverseTransformer = new InverseTransformer();
         UpDownTransformer myUpDownTransformer = new UpDownTransformer();
